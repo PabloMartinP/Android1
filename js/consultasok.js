8745bec0bc4fd2050d04dc85f2562c7d1b49dc5d
Pablo Martin <pablo.penialoza@gmail.com>
2016-10-27 17:18:20 -0300

    v1 dynamic

--- js/consultasok.js
+++ js/consultasok.js
@@ -1,6 +1,8 @@
+//consulta.prop: {codigo, nombreTabla, nombreAMostrar, filtro[]{.nombre, tipo, filtrar:bool }, orden, ordenBy, columnaCodigo}
 var _clave = "";
 var _params = "";
 var _url = "";
+var _consultas = [];
 
 //para que no tarde el click
 window.addEventListener('load', function() {
@@ -8,22 +10,17 @@ window.addEventListener('load', function() {
 }, false);
 
 function actualizar(){
-	//alert("JJJJJJJJ");	
-
+/*
+	var params = $("#param-ok").val();	
+	function _proc(resultado){	alert("proceasndoooojjjjjjjjj ok " + params);}
+	var url = _url + WS_OBTENERTABLA;
+	alert("antes ajax");
+	ejecutar_ajax_ws(url, params, "Cargando consultas", _proc);
+	alert("despues ajax");*/
+////////////////////////////////////////////////////
 	$.mobile.changePage( "#page-loading", { transition: "slideup", changeHash: true });
 
-	cargar_posibles_servicios();
-	cargar_practicos_turnos();		
-	cargar_servicios_sin_practico();
-	cargar_servicios_activos();	
-
-	
-	cargar_servicios_activos_subida();	
-
-	cargar_servicios_activos_bajada();	
-
-	cargar_servicios_activos_campana();	
-	
+	obtener_consultas();
 		
 	$.mobile.changePage( "#page-principal", { transition: "slideup", changeHash: true });
 
@@ -47,10 +44,11 @@ function ws_validar_clave(){
 
 function row_datos_nueva(dato){
 	return "<td class='row-dato'>" + dato + "</td>";
-	
-}
+};
 
 function ajax_cargar(url, params, nombre, id_tabla ){
+	//alert("params:::::::::"+params);
+	//alert("url:::::::::"+url);
 	var ajax = $.ajax({
 	url: url,
 	data: params, 
@@ -60,6 +58,7 @@ function ajax_cargar(url, params, nombre, id_tabla ){
 	dataType: 'json',
 	beforeSend: function () {
 		$("#estado").html("Actualizando "+ nombre +" ...");
+		//alert("before send");
 	}
     });
     ajax.done(function (response) {
@@ -100,6 +99,167 @@ function ajax_cargar(url, params, nombre, id_tabla ){
 			$("#"+id_tabla + " thead th").each(function( index ) {
 				tmp_col = $(this);
 				var tmp_col_txt = tmp_col.text().replace(/\s+/g, '').toLowerCase();
+				  //alert( index + ": " + $(this).text() );
+				  columnas.push($(this).text());
+				  //columnas.push(tmp_col_txt);
+
+				  if(tmp_col.hasClass("ver_notas_codigo")){			  	
+				  	col_codigo = tmp_col_txt;
+				  	//alert("Tienen clase ver_nota-codigo col: " + col_codigo);
+				  }
+				  //col = columnas[columna].replace(/\s+/g, '').toLowerCase();
+				});
+			//alert(columnas.join("-"));
+			var data = resultado_json.resultado;
+			for (var i = 0; i < data.length; i++) {
+				var rowData = data[i];
+				//alert("rowData: " + rowData);
+				var row = $("<tr/>");
+				var col;
+				if(primera_vez){
+					//row.append($("<td class='primera'>  </td>"));
+					$("#"+id_tabla).append("<tr class='primera'></tr>"); 		
+					primera_vez = false;
+				}		
+				
+				for(var columna in columnas){					
+					/*
+					 * esto es porque cuando lee las descripciones estan separadas por espacios y en el vector tienen que tener el nombre exacto
+					 * Ej: Fecha Inicio => vector[Fecha Inicio] y deberia ser Fecha Inicio  => vector[fechainicio]
+					 */
+					col = columnas[columna].replace(/\s+/g, '').toLowerCase();
+
+					row.append($(row_datos_nueva(rowData[col])));
+
+					/*
+					alert("columna: " + columna);
+					alert("col:::"+col);
+					alert("primero");
+					for(var key in rowData) {
+					    alert('----key: ' + key + '\n' + '----value: ' + rowData[key]);
+						//rowData[key] = rowData[key].toLowerCase();
+						key = key.toLowerCase();
+					}
+					alert("segundo");
+					for(var key in rowData) {
+					    alert('----key: ' + key + '\n' + '----value: ' + rowData[key]);
+						//rowData[key] = rowData[key].toLowerCase();
+					}
+					alert("fin segundo ");
+						
+					alert("rowData[col]:::"+rowData[col]);
+					alert("rowData[columna]:::"+rowData[columna]);
+					*/
+
+					/*
+					if(rowData[col]!=null){
+						alert(columnas[columna] + ": " + rowData[col]);
+						//row.append($("<td>" + rowData[col] + "</td>"));
+						row.append($(row_datos_nueva(rowData[col])));
+					}
+					else{
+						if(rowData!='[object Object]'){
+							alert("NULLLL " + col);
+							//row.append($("<td>" + rowData + "</td>"));
+							row.append($(row_datos_nueva(rowData)));
+						}else{							
+							//si viene por aca es porque el campo no existe en la consulta que devuelve el ws
+							alert("col_codigo: " + col_codigo + " - rowData[col_codigo]: " + rowData[col_codigo]);
+							var ver_click = "<div onclick=\"mostrar_nota_servicio('"+rowData[col_codigo]+"')\" class='btn_ver_notas'>Notas</<div>";
+							row.append($("<td>" + ver_click+ "</td>"));							
+						}
+					}
+					*/
+				}//fin for		
+				$("#"+id_tabla).append(row); 		
+			}
+			if(!primera_vez){//si es falso, tiene datos, agrego la ultima fila
+				$("#"+id_tabla).append("<tr class='ultimo'></tr>"); 		
+			}
+			//row.append($("<td>  </td>"));							
+			//alert("TERMINADO " + nombre);	
+			////////////////////////////////////////////////////////////////
+			//$("#estado").html(nombre + " OK");
+			$("#estado").html("LatikaIT");
+
+			//$("#"+id_tabla).table("refresh");
+		}
+				
+		
+    });
+    ajax.fail(function (xhr, status) {
+		$("#estado").html( nombre + " ERROR");
+        alert("faillll-" + status + " - Url: " + url + " - params: " + params);
+		if(xhr!=null)
+			alert("xhr.responseText:"+xhr.responseText);
+    });
+    ajax.always(function () {
+		//alert("allways");
+		//$("#estado").html("OKOKOK");
+
+    });
+
+};
+
+function mostrar_nota_servicio(servicio){
+	//selecciono codigo    
+    //var _url = "/serviciows.asmx/ObtenerNotasDeServicio";    
+    ajax_mostrar_notas(servicio);
+};
+
+
+function ejecutar_ajax_ws(url, params, nombre, procesar){
+	var ajax = $.ajax({
+	url: url,
+	data: params, 
+	type: 'POST',
+	contentType: "application/json; charset=utf-8",
+	async: false,
+	dataType: 'json',
+	beforeSend: function () {
+		$("#estado").html("Actualizando "+ nombre +" ...");
+	}
+    });
+    ajax.done(function (response) {
+		//alert("DONEEEEE");
+		//alert(response);		
+		var resultado_json = $.parseJSON(response.d);
+		//codigo | descripcion | resultado
+		//alert("codigo: " + resultado_json.codigo);
+		//alert("descripcion: " + resultado_json.descripcion);
+		//alert("resultado: " + resultado_json.resultado);
+		
+		if(resultado_json.codigo !=100){//100 es ok
+			alert("webService error cod: " + resultado_json.codigo);
+			alert("webService error desc: " + resultado_json.descripcion);
+			alert("res: " + resultado_json);
+			for(var d in resultado_json){
+				alert(d);
+				alert(resultado_json[d]);
+			}
+		}else{
+
+			procesar(resultado_json.resultado);
+
+/*
+			//alert(resultado_json);
+			//alert("fin ok--------------------------------");
+			//onDone(resultado_json.resultado, id_tabla);
+			/////////////////////////////////////////////////////////////////
+			//limpio la tabla 
+			$("#" + id_tabla + " tbody").empty();
+			//$("#tbodyid").empty();
+			var primera_vez = true;
+
+
+			var columnas = [];					
+
+			//traigo las columnas del th para saber el orden en el que se tienen que cargar los datos
+			var col_codigo;//el campo donde esta la columna codigo
+			var tmp_col;
+			$("#"+id_tabla + " thead th").each(function( index ) {
+				tmp_col = $(this);
+				var tmp_col_txt = tmp_col.text().replace(/\s+/g, '').toLowerCase();
 			  //alert( index + ": " + $(this).text() );
 			  columnas.push($(this).text());
 			  //columnas.push(tmp_col_txt);
@@ -124,10 +284,10 @@ function ajax_cargar(url, params, nombre, id_tabla ){
 				}		
 				
 				for(var columna in columnas){					
-					/*
-					 * esto es porque cuando lee las descripciones estan separadas por espacios y en el vector tienen que tener el nombre exacto
-					 * Ej: Fecha Inicio => vector[Fecha Inicio] y deberia ser Fecha Inicio  => vector[fechainicio]
-					 */
+					
+					 //esto es porque cuando lee las descripciones estan separadas por espacios y en el vector tienen que tener el nombre exacto
+					 // Ej: Fecha Inicio => vector[Fecha Inicio] y deberia ser Fecha Inicio  => vector[fechainicio]
+					 
 					col = columnas[columna].replace(/\s+/g, '').toLowerCase();
 					//alert("columna: " + columna);
 					if(rowData[col]!=null){
@@ -135,12 +295,12 @@ function ajax_cargar(url, params, nombre, id_tabla ){
 						//row.append($("<td>" + rowData[col] + "</td>"));
 						row.append($(row_datos_nueva(rowData[col])));
 						//para_seleccionar_codigo
-						/*
-						if(col=="codigo")
-							row.append($("<td class='para_seleccionar_codigo'>" + rowData[col] + "</td>"));
-						else
-							row.append($("<td>" + rowData[col] + "</td>"));
-						*/
+						
+						//if(col=="codigo")
+						//	row.append($("<td class='para_seleccionar_codigo'>" + rowData[col] + "</td>"));
+						//else
+						//	row.append($("<td>" + rowData[col] + "</td>"));
+						
 					}
 					else{
 						if(rowData!='[object Object]'){
@@ -160,75 +320,154 @@ function ajax_cargar(url, params, nombre, id_tabla ){
 			if(!primera_vez){//si es falso, tiene datos, agrego la ultima fila
 				$("#"+id_tabla).append("<tr class='ultimo'></tr>"); 		
 			}
+*/
+
 			//row.append($("<td>  </td>"));							
 			//alert("TERMINADO " + nombre);	
 			////////////////////////////////////////////////////////////////
-			$("#estado").html(nombre + " OK");
+			//$("#estado").html(nombre + " OK");
+			$("#estado").html("LatikaIT");
+			//$("#"+id_tabla).table("refresh");
 		}
 				
 		
     });
     ajax.fail(function (xhr, status) {
 		$("#estado").html( nombre + " ERROR");
-        alert("fail-" + status);
+        alert("faillll-" + status + " - Url: " + url + " - params: " + params);
     });
     ajax.always(function () {
 		//alert("allways");
 		//$("#estado").html("OKOKOK");
-    });	
-};
 
-function mostrar_nota_servicio(servicio){
-	//selecciono codigo    
-    //var _url = "/serviciows.asmx/ObtenerNotasDeServicio";    
-    ajax_mostrar_notas(servicio);
-};
-
-function cargar_posibles_servicios(){
-	var url = _url + WS_POSIBLES_SERVICIOS;		
-	//alert("ajax antes");	
-	ajax_cargar(url, _params, "Posibles servicios", "tabla-posibles-servicios");
-	//alert("ajax despues ");	
-};
-
-function cargar_servicios_activos(){
-	var url = _url + WS_SERVICIOSACTIVOS;
-	ajax_cargar(url, _params, "Servicios activos", "tabla-servicios-activos");
+    });
 }
 
-function cargar_servicios_activos_subida(){
-	var url = _url + WS_SERVICIOSACTIVOS_SUBIDA;
-	//alert(url);
-	ajax_cargar(url, _params, "Servicios - Subida", "tabla-servicios-activos-subida");
-}
 
-function cargar_servicios_activos_bajada(){
-	var url = _url + WS_SERVICIOSACTIVOS_BAJADA;
-	//alert(url);
-	ajax_cargar(url, _params, "Servicios - Bajada", "tabla-servicios-activos-bajada");
-}
-
-function cargar_servicios_activos_campana(){
-	var url = _url + WS_SERVICIOSACTIVOS_CAMPANA;
-	//alert(url);
-	ajax_cargar(url, _params, "Servicios - Campana", "tabla-servicios-activos-campana");
+//consulta.prop: {codigo, nombreTabla, nombreAMostrar, filtro, orden, ordenBy}
+function consulta_agregar_en_menu(consulta){
+	
+	var nueva_consulta = "<li><a href='#page-"+consulta.nombreTabla+"-"+consulta.codigo+"'  data-transition='slide'>"+consulta.nombreAMostrar+"</a></li>";
+	$("#main-ul-consultas-disponibles").append(nueva_consulta);
 }
 
 
 
-function cargar_practicos_turnos(){	
-	//alert("cargar_practicos_turnos");	
-	var url = _url + WS_PRACTICOS;
-	//alert("ajax antes");
-	ajax_cargar(url, _params, "practicos Turnos", "tabla-practicos-turnos");
-};
 
-function cargar_servicios_sin_practico(){
-	var url = _url + WS_SERVICIOSSINPRACTICO;
-	//alert("ajax antes Servicios sin practico");
-	ajax_cargar(url, _params, "Servicios sin practico", "tabla-servicios-sin-practico");
-	//alert("ajax despues Servicios sin practico");	
-};
+//consulta.prop: {codigo, nombreTabla, nombreAMostrar, filtro[]{.nombre, tipo, filtro}, orden, ordenBy}
+function consulta_agregar_page(consulta){
+	//alert("pag OKe");
+	var page = "";
+	//var template = "<h1>{{firstName}} {{lastName}}</h1>Blog: {{blogURL}}";
+	//var html = Mustache.to_html(template, person);
+	//$('#sampleArea').html(html);
+	var id = consulta_get_id_html(consulta);
+
+	//page = $("body").append($("<div>").append($("<div>").append($("<h1>tiulo</h1>"))).append($("<div>contenttt</div>"))	);​
+	var page = "														\
+	<div data-role='page' id='"+id+"'>     \
+		<div data-role='header' data-position='fixed'>                    \
+			<a href='#' class='ui-btn ui-icon-arrow-l ui-btn-icon-left ui-btn-icon-notext' data-rel='back' data-transition='slide'>Volver</a>   \
+			<h1>"+consulta.nombreAMostrar+"</h1>	\
+			<button id='actualizar' class='ui-btn ui-icon-refresh ui-btn-icon-left ui-btn-icon-notext' >Actualizar</button>		\
+		</div>		\
+			<div data-role='main' class='ui-content'>		";
+				
+	
+
+	//alert("len filter" + consulta.filtro.length);
+	if(consulta.filtro.length>0){
+		page = page + "\
+			<div data-role='collapsible' class='filtros-colapsable'>		\
+				<h1>Filtros</h1>";
+		$.each(consulta.filtro, function(index, filtro){
+			//alert("filtrooooo: "+filtro.filtrar);
+			if(filtro.filtrar){
+				var id_filtro = id + "-filtro-campo" + filtro.nombre.replace(" ", "");
+				page =page+"\
+				<div class='ui-field-contain filtro-para-seleccion' >		\
+					<label for='"+id_filtro+"'>"+filtro.nombre+"</label>	\
+					<input type='text' id='"+id_filtro+"' campo='"+filtro.nombre+"' tipo='"+filtro.tipo+"' condicion='"+filtro.condicion+"' data-type='search' placeholder='"+filtro.nombre+" ...'>	\
+				</div>";
+			}
+		});
+		page = page + "</div>";
+	}
+
+
+	var id_btn_buscar = id+"-filtro-buscar";
+	var id_tabla = consulta_get_id_tabla_html(consulta);
+	
+	//alert("okok");
+	page = page + "\
+		<button class='ui-btn ui-icon-search ui-btn-icon-left boton-buscar-datos' codigo='"+consulta.codigo+"' id='"+id_btn_buscar+"'>Buscar</button>		\
+				<table  id='"+id_tabla+"' data-role='table' class='ui-responsive ui-shadow agregar_ver_notas' >		\
+					<thead>		\
+					<tr>		";
+	//alert("OKOKOKOKKOKO");
+
+	//cargo las columnas
+	$.each(consulta.filtro, function(index, columna){
+		if(consulta.columnaCodigo.toLowerCase() == columna.nombre.toLowerCase()){
+			//alert("encontro el codigo:"+columna.nombre);
+			page = page + "<th class='ver_notas_codigo' >"+columna.nombre+"</th>";
+		}
+		else{
+			page = page + "<th>"+columna.nombre+"</th>";
+		}
+	});
+
+	page = page + "\
+					</tr>		\
+					</thead>	\
+					<tbody>		\
+					</tbody>	\
+				</table>		\
+			</div>		\
+			<div data-role='footer'  class='centrado' data-position='fixed'>		\
+				LatikaIT		\
+			</div>		\
+		</div>";
+
+	//alert(page);
+	//$("body").addClass("asdfadfasdfasd");
+	$("body").append(page);
+}
+function obtener_consultas(){
+
+	function _proc(resultado){
+		//alert("proceasndoooo" + tabla);
+
+		_consultas = resultado;
+
+		if(_consultas.length>0){
+			for (var i = 0; i < _consultas.length; i++) {
+				//alert("varr " + i);
+				var consulta = _consultas[i];
+
+				consulta_agregar_en_menu(consulta);
+				consulta_agregar_page(consulta);
+
+				//for(var key in rowData) alert('----key: ' + key + '\n' + '----value: ' + rowData[key]);
+				
+				//alert( rowData);
+			}
+		}
+
+		
+		//alert("FIn proceasndoooooo ");
+	}
+
+
+
+	var url = _url + WS_OBTENERCONSULTAS;
+	//alert("url:OBETNERCONSULTASS:"+url);
+	//alert("antes ajax");
+	ejecutar_ajax_ws(url, _params, "Cargando consultas", _proc);
+	//alert("despues ajax");
+
+}
+
 
 function ajax_mostrar_notas(servicio) {
 		var url = _url + WS_NOTASDESERVICIO;
@@ -289,25 +528,120 @@ function ajax_mostrar_notas(servicio) {
         ajax.fail(function(xhr, status) {
             //$("#estado").html(nombre + " ERROR");
             //alert(xhr);
-            alert("fail-" + status + "\nUrl: " + url);
+            alert("faillll-" + status + " - Url: " + url + " - params: " + params);
         });
         ajax.always(function() {
             //alert("allways");
             //$("#estado").html("OKOKOK");
         });
-    };
+};
 
-
-
+function consulta_get_id_tabla_html(consulta){
+	 return consulta_get_id_html(consulta)+"-tabla";
+}
+function consulta_get_id_html(consulta){
+	return "page-"+consulta.nombreTabla + "-" + consulta.codigo;
+}
 
 $(document).ready(function(){
 	//alert("ready-indexok.js");
 	_clave = acceso_ws_get_clave();
 	_params = "{acceso: '"+_clave+"'}";
 	_url = acceso_ws_get_url();
-
-	//alert("clave: " + _clave + " - params: " + _params + " - Url: " + _url);
-	actualizar();
+	//alert("_url:"+_url);
+	_consultas = [];
 	
+
+	
+	actualizar();
+
+	//alert("finallllll");
+
+	$(".boton-buscar-datos").click(function(){
+		//alert("hokkk");
+
+		var consulta_codigo = $(this).attr("codigo");
+		//alert(consulta_codigo);
+
+		//alert("lenn"+_consultas.length);
+
+		$.grep(_consultas, function(consulta, index){
+			
+
+			//BUSCO LA CONSULTA
+			//alert("compare: " + consulta.codigo + "__" + consulta_codigo);
+			if(consulta.codigo != consulta_codigo)
+				return;
+
+			//alert(consulta.codigo);
+			//alert(consulta.nombreTabla);
+			
+			var id = "#" +  consulta_get_id_html(consulta);
+			//alert(id);
+
+			//$(id + " .filtro-para-seleccion input").addClass("JJJJJJJJJ");
+			var where = "";
+			
+			$(id + " .filtro-para-seleccion input").each(function(){
+				//alert($(this).val());
+				//alert("attr-"+$(this).attr("campo"));
+				
+				var valor = $(this).val();
+				var campo = $(this).attr("campo");
+				var tipo = $(this).attr("tipo");
+				var condicion = $(this).attr("condicion");
+				//alert("condicion:"+condicion);
+
+				if(valor!=""){
+					//si es un int
+					if(tipo.indexOf("int") >= 0 ){
+						//where = where + campo + " = \'"+-1+"\' and ";	
+						switch(condicion){
+						case "1":// int e igual = 
+							where = where + campo + " = "+valor+" and ";	
+							break;
+						case "2"://int like '%' NO IMPLEMENTADO
+							//here = where + campo + " like '+"valor"+%' ";	
+							break;
+						}						
+					}else{
+						//es un string!
+						switch(condicion){
+						case "1":// string e igual = 
+							where = where + campo + " = '"+valor+"' and ";	
+							break;
+						case "2"://string like '%' 
+							where = where + campo + " like '"+valor+"%' and ";	
+							break;
+						}						
+					}
+				}
+				//alert("where::::::"+where);
+				
+			});
+
+			where = where + " 1 = 1 ";
+
+			//alert("where:"+where);
+			var newparams = _params;
+			var url = _url + WS_OBTENERTABLA;
+			newparams = newparams.replace('}', ", tabla:'"+consulta.nombreTabla+"'}");
+			newparams = newparams.replace('}', ", filtro:\"" + where+ "\"}");
+			newparams = newparams.replace('}', ", orden:'"+consulta.orderBy+"'}");
+			//alert("newparams:"+newparams);
+			//$("#page-actores1-7-filtro-campoCodigoActor").val(newparams);
+
+			var id_tabla = consulta_get_id_tabla_html(consulta);
+			//alert("consulta_get_id_tabla_html:"+id_tabla);
+
+			ajax_cargar(url, newparams, consulta.nombreAMostrar +"!", id_tabla);
+			$("#"+id_tabla).table("refresh");
+
+			//alert("TERMINADOOOO");
+		});
+
+
+	});
+
 
 });
